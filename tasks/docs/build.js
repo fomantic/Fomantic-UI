1f5938a30294de5024e7b6ee3e7e998355ebbc01
ColinFrick <colin@frick.io>
2018-10-04 21:58:35 +0200

    build: update docs build to gulp4

--- tasks/docs/build.js
+++ tasks/docs/build.js
@@ -3,74 +3,45 @@
  *******************************/
 
 var
-  gulp         = require('gulp'),
+  gulp        = require('gulp'),
 
   // node dependencies
-  console      = require('better-console'),
-  fs           = require('fs'),
-  map          = require('map-stream'),
+  console     = require('better-console'),
+  fs          = require('fs'),
+  map         = require('map-stream'),
 
   // gulp dependencies
-  autoprefixer = require('gulp-autoprefixer'),
-  chmod        = require('gulp-chmod'),
-  clone        = require('gulp-clone'),
-  flatten      = require('gulp-flatten'),
-  gulpif       = require('gulp-if'),
-  header       = require('gulp-header'),
-  less         = require('gulp-less'),
-  minifyCSS    = require('gulp-clean-css'),
-  plumber      = require('gulp-plumber'),
-  print        = require('gulp-print'),
-  rename       = require('gulp-rename'),
-  replace      = require('gulp-replace'),
-  uglify       = require('gulp-uglify'),
+  print       = require('gulp-print'),
 
   // user config
-  config       = require('../config/docs'),
+  config      = require('../config/docs'),
 
   // install config
-  tasks        = require('../config/tasks'),
-  configSetup  = require('../config/project/config'),
-  install      = require('../config/project/install'),
+  tasks       = require('../config/tasks'),
+  configSetup = require('../config/project/config'),
+  install     = require('../config/project/install'),
 
   // metadata parsing
-  metadata     = require('./metadata'),
+  metadata    = require('./metadata'),
 
   // build methods
-  buildJS      = require('../build/javascript').buildJS,
-  buildCSS     = require('../build/css').buildCSS,
-  buildAssets  = require('../build/assets').buildAssets,
-
+  buildJS     = require('../build/javascript').buildJS,
+  buildCSS    = require('../build/css').buildCSS,
+  buildAssets = require('../build/assets').buildAssets,
 
   // shorthand
-  globs,
-  assets,
-  output,
-  source,
-
-  banner       = tasks.banner,
-  comments     = tasks.regExp.comments,
-  log          = tasks.log,
-  settings     = tasks.settings
+  log         = tasks.log
 ;
 
 
 module.exports = function (callback) {
 
-  var
-    stream,
-    compressedStream,
-    uncompressedStream
-  ;
-
   // use a different config
   config = configSetup.addDerivedValues(config);
 
   // shorthand
-  globs  = config.globs;
-  assets = config.paths.assets;
-  output = config.paths.output;
-  source = config.paths.source;
+  const globs  = config.globs;
+  const output = config.paths.output;
 
   /*--------------
    Parse metadata
@@ -81,34 +52,41 @@ module.exports = function (callback) {
   // repository is present and in proper directory location as
   // specified by docs.json.
   console.info('Building Metadata');
-  gulp.src(config.paths.template.eco + globs.eco)
-    .pipe(map(metadata.parser))
-    .on('end', function () {
-      fs.writeFile(output.metadata + '/metadata.json', JSON.stringify(metadata.result, null, 2), new Function());
-    })
-  ;
+
+  function buildMetaData() {
+    return gulp.src(config.paths.template.eco + globs.eco)
+      .pipe(map(metadata.parser))
+      .on('end', function () {
+        fs.writeFile(output.metadata + '/metadata.json', JSON.stringify(metadata.result, null, 2), new Function());
+      });
+  }
 
   /*--------------
     Copy Examples
   ---------------*/
-
   console.info('Copying examples');
-  // copy src/ to server
-  gulp.src('examples/**/*.*')
-    .pipe(gulp.dest(output.examples))
-    .pipe(print(log.created))
-  ;
+
+  function copyExample() {
+    // copy src/ to server
+    return gulp.src('examples/**/*.*')
+      .pipe(gulp.dest(output.examples))
+      .pipe(print(log.created));
+  }
+
 
   /*--------------
      Copy Source
   ---------------*/
-
   console.info('Copying LESS source');
-  // copy src/ to server
-  gulp.src('src/**/*.*')
-    .pipe(gulp.dest(output.less))
-    .pipe(print(log.created))
-  ;
+
+  function copyLess() {
+    // copy src/ to server
+
+    return gulp.src('src/**/*.*')
+      .pipe(gulp.dest(output.less))
+      .pipe(print(log.created));
+  }
+
 
   /*--------------
         Build
@@ -122,10 +100,15 @@ module.exports = function (callback) {
     return;
   }
 
-  gulp.parallel(
-    (callback) => buildJS('docs', config, callback),
-    (callback) => buildCSS('docs', false, config, {}, callback),
-    (callback) => buildAssets(config, callback)
+  gulp.series(
+    buildMetaData,
+    copyExample,
+    copyLess,
+    gulp.parallel(
+      (callback) => buildJS('docs', config, callback),
+      (callback) => buildCSS('docs', false, config, {}, callback),
+      (callback) => buildAssets(config, callback)
+    )
   )(callback);
 
 };
