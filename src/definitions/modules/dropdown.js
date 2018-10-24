63bc9a80cdf867675337bd39f1c75d27c301ebe2
Marco 'Lubber' Wienkoop <c64@lubber.de>
2018-10-24 09:10:06 +0200

    fix(dropdown): IE11 got focus on submenu selection again

--- src/definitions/modules/dropdown.js
+++ src/definitions/modules/dropdown.js
@@ -1268,9 +1268,7 @@ $.fn.dropdown = function(parameters) {
                 isBubbledEvent = ($subMenu.find($target).length > 0)
               ;
               // prevents IE11 bug where menu receives focus even though `tabindex=-1`
-              if(module.has.menuSearch()) {
-                $(document.activeElement).blur();
-              }
+              $(document.activeElement).blur();
               if(!isBubbledEvent && (!hasSubMenu || settings.allowCategorySelection)) {
                 if(module.is.searchSelection()) {
                   if(settings.allowAdditions) {
