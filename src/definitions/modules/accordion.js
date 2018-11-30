532fd5af5264a5558ca35fabd549e346db7cea1f
prudho <pierrick.prudhomme@environnement-recycling.com>
2018-11-30 16:05:33 +0100

    style(accordion): remove unused variable

--- src/definitions/modules/accordion.js
+++ src/definitions/modules/accordion.js
@@ -97,7 +97,7 @@ $.fn.accordion = function(parameters) {
 
         observeChanges: function() {
           if('MutationObserver' in window) {
-            observer = new MutationObserver(function(mutations) {
+            observer = new MutationObserver(function() {
               module.debug('DOM tree modified, updating selector cache');
               module.refresh();
             });
