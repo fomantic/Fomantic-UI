140c61566b1db4ed5072cc803b846c0adcbd0838
iarspider <iarspider@gmail.com>
2019-03-08 13:31:28 +0100

    Update form.js
    [form remove rule] Check if `validation[field] !== undefined` before accessing any of it's properties

--- dist/components/form.js
+++ dist/components/form.js
@@ -764,12 +764,12 @@ $.fn.form = function(parameters) {
                 ? rule
                 : [rule]
             ;
-            if(rule == undefined) {
-              module.debug('Removed all rules');
-              validation[field].rules = [];
+            if(validation[field] === undefined || !Array.isArray(validation[field].rules)) {
               return;
             }
-            if(validation[field] == undefined || !Array.isArray(validation[field].rules)) {
+            if(rule === undefined) {
+              module.debug('Removed all rules');
+              validation[field].rules = [];
               return;
             }
             $.each(validation[field].rules, function(index, rule) {
