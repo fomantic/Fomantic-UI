00b619757753e207c730f203901cb703d252888b
exoego <mogami@exoego.net>
2019-03-10 13:57:18 +0900

    chore(form): no-mixed-operators

--- src/definitions/behaviors/form.js
+++ src/definitions/behaviors/form.js
@@ -1367,7 +1367,7 @@ $.fn.form.settings = {
 
     // is not empty or blank string
     empty: function(value) {
-      return !(value === undefined || '' === value || Array.isArray(value) && value.length === 0);
+      return !(value === undefined || '' === value || (Array.isArray(value) && value.length === 0));
     },
 
     // checkbox checked
