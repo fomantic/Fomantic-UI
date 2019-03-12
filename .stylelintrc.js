cbad73056984c404fec20f2b66a665982be52198
exoego <mogami@exoego.net>
2019-03-12 11:30:45 +0900

    chore(stylelint): Document stylelint configuration

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -2,8 +2,11 @@
 const SUI_SPECIFIC_AT_MARK_VARS = ['type', 'element'];
 
 module.exports = {
-  // 'extends': 'stylelint-config-standard',
+  'extends': 'stylelint-config-standard',
   'rules': {
+    // -----------------------------------------------------------------------------------------------------------------
+    // Overrides 'standard' rules to suit the present format
+    // -----------------------------------------------------------------------------------------------------------------
     'at-rule-empty-line-before': ['always', {
       except: [
         'blockless-after-same-name-blockless',
@@ -24,15 +27,19 @@ module.exports = {
       ignore: ['after-comment'],
     }],
     'selector-pseudo-element-colon-notation': 'single',
-    // TODO: Enable with postcss-less https://github.com/stylelint/stylelint/issues/2748
-    'selector-max-empty-lines': null,
-    'selector-descendant-combinator-no-non-space': null,
-    // disable
+
+    // -----------------------------------------------------------------------------------------------------------------
+    // Consider enabling (remove each line and follow `standard` rule)
+    // -----------------------------------------------------------------------------------------------------------------
     'declaration-colon-space-before': null,
     'declaration-block-no-duplicate-properties': null,
     'declaration-empty-line-before': null,
     'max-empty-lines': null,
     'no-descending-specificity': null,
     'no-duplicate-selectors': null,
+    // TODO: Enable with postcss-less https://github.com/stylelint/stylelint/issues/2748
+    'selector-max-empty-lines': null,
+    // TODO: Enable with postcss-less https://github.com/stylelint/stylelint/issues/2748
+    'selector-descendant-combinator-no-non-space': null,
   }
 };
