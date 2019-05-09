6cc22db14bdfcf79c941aeb0604a400f09877b58
Sean Hamilton <sean@hamistudios.com>
2019-05-09 18:24:20 +0100

    chore(release): update dist

--- dist/components/embed.js
+++ dist/components/embed.js
@@ -1,6 +1,6 @@
 /*!
- * # Semantic UI 2.7.2 - Embed
- * http://github.com/semantic-org/semantic-ui/
+ * # Fomantic-UI - Embed
+ * http://github.com/fomantic/Fomantic-UI/
  *
  *
  * Released under the MIT license
