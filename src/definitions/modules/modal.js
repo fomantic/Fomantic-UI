eb6d320fa329ec4bc1970002e0c055154d775c75
Julien Palmas <julien@crx.io>
2019-06-05 10:39:01 +0200

    Removing traling white spaces
    I don't know if the project coding style guide recommends to remove trailing white spaces, but I get the feeling it does considering the other empty lines in `modal.js`
    
    So just a little PR to avoid my editor editing this line when I hack Fomantic ;-)

--- src/definitions/modules/modal.js
+++ src/definitions/modules/modal.js
@@ -484,7 +484,7 @@ $.fn.modal = function(parameters) {
                     if ( settings.allowMultiple ) {
                       $previousModal.addClass(className.front);
                       $module.removeClass(className.front);
-      
+
                       if ( hideOthersToo ) {
                         $allModals.find(selector.dimmer).removeClass('active');
                       }
