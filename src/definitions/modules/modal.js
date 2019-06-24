d880d35664832786e59da027543bb240607133fa
Euan Goddard <euan@stockopedia.com>
2019-06-24 17:42:54 +0100

    feat(modal): destroy should tear down mutation observer

--- src/definitions/modules/modal.js
+++ src/definitions/modules/modal.js
@@ -155,6 +155,9 @@ $.fn.modal = function(parameters) {
         },
 
         destroy: function() {
+          if (observer) {
+            observer.disconnect();
+          }
           module.verbose('Destroying previous modal');
           $module
             .removeData(moduleNamespace)
