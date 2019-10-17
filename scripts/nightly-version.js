334c0170789da9a3c0fa6dadd56f6a92c5b9ff44
Sean Hamilton <sean@hamistudios.com>
2019-10-17 09:43:01 +0100

    build(scripts): add script to set nightly version

---
+++ scripts/nightly-version.js
@@ -0,0 +1,26 @@
+// node
+const fs = require('fs')
+
+// npm
+const fetch = require('node-fetch')
+
+// pkg
+const pkg = require('../package.json')
+
+const ghBase = 'https://api.github.com'
+const repoUrlPath = 'fomantic/Fomantic-UI'
+
+const run = async function () {
+  return fetch(`${ghBase}/repos/${repoUrlPath}/milestones`)
+    .then(r => r.json())
+    .then(milestones => milestones.filter(m => m.title.indexOf('x') === -1)[0].title)
+}
+
+run()
+  .then(nextVersion => {
+    pkg.version = nextVersion
+  })
+  .then(() => {
+    fs.writeFileSync('../package.json', JSON.stringify(pkg, null, 2))
+  })
+  .then(() => console.log('Done'))
