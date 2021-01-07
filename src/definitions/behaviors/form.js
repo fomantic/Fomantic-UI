fomantic/Fomantic-UI

commit 1408be05462b532f23c7d9cee0c600e030fdbb34

fix(form): refresh on instance to fetch dom changes
When a form gets initialized it is refreshing the field selectors.
This is fine as long as there is only one call to the same form. Whenever a later bahavior call like get values takes place on the same form and the form fields have changed, the previous stored instance is not getting refresh (because only the "new" module is)

src/definitions/behaviors/form.js
@@ -627,7 +627,7 @@ $.fn.form = function(parameters) {
             instance = $module.data(moduleNamespace);
 
             // refresh selector cache
-            module.refresh();
+            (instance || module).refresh();
           },
           field: function(identifier) {
             module.verbose('Finding field with identifier', identifier);

commit 0949b16fdbb99fbddd6a4a159e73518e5d536a98

fix(form): refresh module on re-initializaion
This PR fixes a regression from #1811 whereas the fields are not refreshed anymore when a form gets initialized a second/multiple times

src/definitions/behaviors/form.js
@@ -91,6 +91,7 @@ $.fn.form = function(parameters) {
           else {
             if(instance !== undefined) {
               instance.invoke('destroy');
+              module.refresh();
             }
             module.verbose('Initializing form validation', $module, settings);
             module.bindEvents();