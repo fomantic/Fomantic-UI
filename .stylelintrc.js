ce26829e4dd5d959410643b70a5d403367f6283d
Michael Voříšek <mvorisek@mvorisek.cz>
2022-12-10 22:06:57 +0100

    feat(lint): add stylelint overrides and variation files
    In #2592 I completely missed there are also LESS files named with *.overrides and *.variables extensions, so these LESS files are fixed in this PR. As in #2592, only whitespaces are fixed, no functional change.

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -8,39 +8,52 @@ module.exports = {
         'test/coverage/**',
     ],
     rules: {
-        indentation: 4,
         'at-rule-empty-line-before': null,
-        'block-closing-brace-newline-before': null,
-        'declaration-block-trailing-semicolon': null, // bad formatting with "each()"
-        'declaration-empty-line-before': null,
+        'block-closing-brace-newline-before': null, // fix bad formatting with "each()"
+        'declaration-block-trailing-semicolon': null, // fix bad formatting with "each()"
+        indentation: null, // TODO change to "4" once https://github.com/fomantic/Fomantic-UI/pull/2593#discussion_r1045131096 is fixed
         'max-line-length': null,
+        'no-descending-specificity': null,
+        'no-extra-semicolons': null, // fix GH-1832 - workaround for wikimedia/less.php parser
         'rule-empty-line-before': null,
 
         // TODO
-        'no-descending-specificity': null, // 11532 errors
-        'property-case': null, // 338 errors
-        'string-quotes': null, // 249 errors
-        'no-duplicate-selectors': null, // 212 errors
+        'string-quotes': null, // 1676 errors
+        'at-rule-no-unknown': null, // 1081 errors
+        'property-case': null, // 866 errors
+        'at-rule-name-case': null, // 764 errors
+        'no-duplicate-selectors': null, // 224 errors
         'selector-not-notation': null, // 169 errors
         'no-invalid-position-at-import-rule': null, // 104 errors
-        'at-rule-no-unknown': null, // 104 errors
-        'import-notation': null, // 55 errors
-        'function-no-unknown': null, // 43 errors
-        'property-no-vendor-prefix': null, // 31 errors
+        'function-no-unknown': null, // 67 errors
+        'length-zero-no-unit': null, // 64 errors
+        'import-notation': null, // 56 errors
+        'keyframes-name-pattern': null, // 50 errors
+        'property-no-vendor-prefix': null, // 49 errors
+        'number-leading-zero': null, // 49 errors
+        'color-function-notation': null, // 32 errors
+        'alpha-value-notation': null, // 32 errors
+        'color-hex-case': null, // 32 errors
+        'block-no-empty': null, // 25 errors
         'selector-no-vendor-prefix': null, // 19 errors
-        'number-leading-zero': null, // 19 errors
+        'selector-class-pattern': null, // 19 errors
+        'number-no-trailing-zeros': null, // 16 errors
+        'font-family-name-quotes': null, // 13 errors
         'declaration-block-no-redundant-longhand-properties': null, // 12 errors
-        'value-keyword-case': null, // 11 errors
+        'value-keyword-case': null, // 12 errors
         'value-no-vendor-prefix': null, // 9 errors
-        'keyframes-name-pattern': null, // 9 errors
-        'color-function-notation': null, // 8 errors
-        'alpha-value-notation': null, // 8 errors
+        'color-hex-length': null, // 9 errors
+        'no-empty-source': null, // 8 errors
+        'font-family-no-missing-generic-family-keyword': null, // 8 errors
+        'number-max-precision': null, // 7 errors
         'selector-attribute-quotes': null, // 7 errors
-        'number-no-trailing-zeros': null, // 6 errors
         'shorthand-property-no-redundant-values': null, // 5 errors
+        'function-url-quotes': null, // 5 errors
         'declaration-block-no-duplicate-properties': null, // 2 errors
-        'number-max-precision': null, // 2 errors
-        'color-hex-case': null, // 1 error
-        'color-hex-length': null, // 1 error
+        'font-family-no-duplicate-names': null, // 2 errors
+        'function-linear-gradient-no-nonstandard-direction': null, // 1 error
+        'at-rule-no-vendor-prefix': null, // 1 error
+        'declaration-block-no-shorthand-property-overrides': null, // 1 error
     },
+    reportNeedlessDisables: true,
 };
