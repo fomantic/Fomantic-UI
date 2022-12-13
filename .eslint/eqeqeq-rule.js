6f7074827a1d7de004be3d04eb260fdd7ddb1fbc
Michael Voříšek <mvorisek@mvorisek.cz>
2022-12-13 19:14:56 +0100

    add custom eqeqeq rule

---
+++ .eslint/eqeqeq-rule.js
@@ -0,0 +1,19 @@
+const eslint = require('eslint');
+const ruleComposer = require('eslint-rule-composer');
+
+const rule = new eslint.Linter().getRules().get('eqeqeq');
+
+module.exports = ruleComposer.filterReports(
+    rule,
+    (problem) => {
+        if (problem.node.type === 'BinaryExpression'
+            && (problem.node.operator === '==' || problem.node.operator === '!=')
+            && problem.node.right.type === 'Literal'
+            && problem.node.right.raw !== '\'\''
+        ) {
+            return problem;
+        }
+
+        return false;
+    }
+);
--- .eslint/index.js
+++ .eslint/index.js
@@ -1,5 +1,6 @@
 module.exports = {
     rules: {
+        eqeqeq: require('./eqeqeq-rule'),
         'no-extra-parens': require('./no-extra-parens-rule'),
     },
     configs: {
@@ -8,6 +9,8 @@ module.exports = {
                 '@internal/eslint-plugin',
             ],
             rules: {
+                // fixes only variable == (or !=) 'non-empty-string-scalar'
+                '@internal/eqeqeq': ['error', 'always'],
                 // https://github.com/eslint/eslint/issues/16626
                 // https://github.com/airbnb/javascript/blob/eslint-config-airbnb-v19.0.4/packages/eslint-config-airbnb-base/rules/errors.js#L66
                 '@internal/no-extra-parens': ['error', 'all', {
