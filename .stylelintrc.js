4ca9225a93b136b751be3d6b658f092f083d30a9
Michael Voříšek <mvorisek@mvorisek.cz>
2022-12-14 23:31:33 +0100

    case/unknown errors are impossible to fix

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -9,29 +9,30 @@ module.exports = {
     ],
     rules: {
         'at-rule-empty-line-before': null,
+        'at-rule-name-case': null,
+        'at-rule-no-unknown': null,
         'block-closing-brace-newline-before': null, // fix bad formatting with "each()"
         'declaration-block-trailing-semicolon': null, // fix bad formatting with "each()"
+        'function-no-unknown': null,
         indentation: null, // TODO change to "4" once https://github.com/fomantic/Fomantic-UI/pull/2593#discussion_r1045131096 is fixed
         linebreaks: 'unix',
         'max-line-length': null,
         'no-descending-specificity': null,
         'no-extra-semicolons': null, // fix GH-1832 - workaround for wikimedia/less.php parser
+        'no-duplicate-selectors': null,
         'number-max-precision': 5,
+        'property-case': null,
         'rule-empty-line-before': null,
         'string-quotes': null,
+        'value-keyword-case': null,
 
         // TODO rules to be removed/fixed in v2.10.0 as fixes are not compatible with IE11
         'alpha-value-notation': 'number', // https://caniuse.com/mdn-css_properties_opacity_percentages
         'color-function-notation': 'legacy', // https://caniuse.com/mdn-css_types_color_rgba_space_separated_parameters
 
         // TODO
-        'at-rule-no-unknown': null, // 1081 errors
-        'property-case': null, // 866 errors
-        'at-rule-name-case': null, // 764 errors
-        'no-duplicate-selectors': null, // 224 errors
         'selector-not-notation': null, // 169 errors
         'no-invalid-position-at-import-rule': null, // 104 errors
-        'function-no-unknown': null, // 67 errors
         'import-notation': null, // 56 errors
         'keyframes-name-pattern': null, // 50 errors
         'property-no-vendor-prefix': null, // 49 errors
@@ -40,7 +41,6 @@ module.exports = {
         'selector-class-pattern': null, // 19 errors
         'font-family-name-quotes': null, // 13 errors
         'declaration-block-no-redundant-longhand-properties': null, // 12 errors
-        'value-keyword-case': null, // 12 errors
         'value-no-vendor-prefix': null, // 9 errors
         'font-family-no-missing-generic-family-keyword': null, // 8 errors
         'selector-attribute-quotes': null, // 7 errors
