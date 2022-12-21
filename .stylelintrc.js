b3dc749053661539c02d33c7dd98810b9a0cd00e
Michael Voříšek <mvorisek@mvorisek.cz>
2022-12-21 22:58:19 +0100

    feat(lint): add prettier formatter to assert LESS files whitespaces
    Compared to Stylelint, Prettier is very simple tool and I am quite supprised Stylelint is deprecating the whitespace rules in favor of Prettier. For LESS it seems ok, for JS, we should never enable Prettier as formatting is part of a code and improves readability.
    
    As Prettier is an opinionated formatter instead of an linter, we need to satisfy it everywhere without exceptions.
    
    When I was crafting this PR, I found several Prettier issues and proposed changes to Prettier:
    
    Fix semicolon duplicated at the end of LESS file prettier/prettier#14007
    Fix no space after unary minus when followed by opening parenthesis in LESS prettier/prettier#14008
    Do not change case of property name if inside a variable declaration in LESS prettier/prettier#14034
    In this PR, these changes are contained and Prettier is patched before it is run. Once the changes are merged in prettier and stable release is made, they can be removed.
    
    This PR fixes minor whitespaces unfixed/undetected in GH-2610.
    
    And also asserts:
    
    PHP port of the LESS fails to compile colors.less #1832
    feat(lint): add stylelint overrides and variation files #2593 (comment)
    Prettier has no built in support to dump the diff - prettier/prettier#6885 - so we dump it in the CI using git.

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -11,16 +11,16 @@ module.exports = {
         'at-rule-empty-line-before': null,
         'at-rule-name-case': null,
         'at-rule-no-unknown': null,
-        'block-closing-brace-newline-before': null, // fix bad formatting with "each()"
         'color-function-notation': 'legacy',
         'color-hex-case': 'lower',
-        'declaration-block-trailing-semicolon': null, // fix bad formatting with "each()"
+        'declaration-colon-newline-after': null, // handled by Prettier
         'function-no-unknown': null,
-        indentation: null, // TODO change to "4" once https://github.com/fomantic/Fomantic-UI/pull/2593#discussion_r1045131096 is fixed
+        'import-notation': null,
+        indentation: null, // handled by Prettier
         linebreaks: 'unix',
         'max-line-length': null,
         'no-descending-specificity': null,
-        'no-extra-semicolons': null, // fix GH-1832 - workaround for wikimedia/less.php parser
+        'no-extra-semicolons': null, // handled by Prettier
         'no-duplicate-selectors': null,
         'number-max-precision': 5,
         'property-case': null,
@@ -38,10 +38,9 @@ module.exports = {
 
         // TODO rules to be removed/fixed in v2.10.0 as fixes are not compatible with IE11
         'alpha-value-notation': 'number', // https://caniuse.com/mdn-css_properties_opacity_percentages
+        'selector-not-notation': null, // https://caniuse.com/css-not-sel-list
 
         // TODO
-        'selector-not-notation': null, // 169 errors
-        'import-notation': null, // 56 errors
         'keyframes-name-pattern': null, // 50 errors
         'block-no-empty': null, // 25 errors
         'selector-no-vendor-prefix': null, // 19 errors
