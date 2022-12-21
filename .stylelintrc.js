e3b281ba9eff6b92adb33e451cbb8e6a84694614
Michael Voříšek <mvorisek@mvorisek.cz>
2022-12-22 00:50:19 +0100

    fix (wrong) function-linear-gradient-no-nonstandard-direction manually

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -48,7 +48,6 @@ module.exports = {
         'declaration-block-no-redundant-longhand-properties': null, // 12 errors
         'value-no-vendor-prefix': null, // 9 errors
         'font-family-no-missing-generic-family-keyword': null, // 8 errors
-        'function-linear-gradient-no-nonstandard-direction': null, // 1 error
     },
     reportNeedlessDisables: true,
 };
