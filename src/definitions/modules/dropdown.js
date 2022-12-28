703fa06cb7b07114e4c1c206a014702de6f9f15d
lubber-de <c64@lubber.de>
2022-12-28 19:15:40 +0100

    fix(dropdown): dont open dropdown again on forceselection

--- src/definitions/modules/dropdown.js
+++ src/definitions/modules/dropdown.js
@@ -1361,7 +1361,7 @@
                             var
                                 $choice        = $(this),
                                 $target        = event
-                                    ? $(event.target)
+                                    ? $(event.target || '')
                                     : $(''),
                                 $subMenu       = $choice.find(selector.menu),
                                 text           = module.get.choiceText($choice),
@@ -1379,7 +1379,7 @@
                                         module.remove.userAddition();
                                     }
                                     module.remove.filteredItem();
-                                    if (!module.is.visible()) {
+                                    if (!module.is.visible() && $target.length > 0) {
                                         module.show();
                                     }
                                     module.remove.searchTerm();
