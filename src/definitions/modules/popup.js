94270ba965c4eccb6c1a0ef97e7b5c16438225c4
lubber-de <c64@lubber.de>
2023-03-19 17:18:27 +0100

    feat(popup): linting fixes

--- src/definitions/modules/popup.js
+++ src/definitions/modules/popup.js
@@ -818,12 +818,12 @@
                         if (searchDepth === settings.maxSearchDepth && typeof settings.lastResort === 'string') {
                             position = settings.lastResort;
                         }
-            if(position === 'top right' || position === 'bottom right') {
-              var possibleMargin = calculations.boundary.right - calculations.parent.width;
-              if(possibleMargin > 0) {
-                offset -= possibleMargin;
-              }
-            }
+                        if (position === 'top right' || position === 'bottom right') {
+                            var possibleMargin = calculations.boundary.right - calculations.parent.width;
+                            if (possibleMargin > 0) {
+                                offset -= possibleMargin;
+                            }
+                        }
                         switch (position) {
                             case 'top left': {
                                 positioning = {
