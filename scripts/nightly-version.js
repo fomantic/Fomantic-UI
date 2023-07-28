73206fa8db94046ca39882b3f847cdbf397cd73d
lubber-de <c64@lubber.de>
2023-07-28 11:12:17 +0200

    fix(lint): satisfy linting for import order

--- scripts/nightly-version.js
+++ scripts/nightly-version.js
@@ -3,14 +3,14 @@ const fs = require('fs');
 const path = require('path');
 const childProcess = require('child_process');
 
+// pkg
+const process = require('process');
+const pkg = require('../package.json');
+
 // npm
 const fetch = require('node-fetch'); // eslint-disable-line import/no-extraneous-dependencies
 const semver = require('semver'); // eslint-disable-line import/no-extraneous-dependencies
 const actions = require('@actions/core');
-
-// pkg
-const process = require('process');
-const pkg = require('../package.json');
 
 const ghBase = 'https://api.github.com';
 const repoUrlPath = 'fomantic/Fomantic-UI';
