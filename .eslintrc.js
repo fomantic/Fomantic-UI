3beb3773ea3327a68ca861b58007c893a6dd9c9e
Michael Voříšek <mvorisek@mvorisek.cz>
2023-12-19 12:49:33 +0100

    disable all failing rules with todo

--- .eslintrc.js
+++ .eslintrc.js
@@ -126,5 +126,19 @@ module.exports = {
         extends: [
             'plugin:@typescript-eslint/recommended',
         ],
+        rules: {
+            // TODO rules with a lot of errors to be fixed manually, fix in a separate PR
+            '@typescript-eslint/no-explicit-any': 'off',
+            '@typescript-eslint/triple-slash-reference': 'off',
+
+            // TODO
+            'camelcase': 'off',
+            'no-alert': 'off',
+            'no-use-before-define': 'off',
+            '@internal/eqeqeq': 'off',
+            '@typescript-eslint/ban-types': 'off',
+            '@typescript-eslint/no-empty-function': 'off',
+            '@typescript-eslint/no-unused-vars': 'off',
+        }
     }],
 };
