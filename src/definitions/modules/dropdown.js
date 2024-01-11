8671bdf52aa05cd6001b708ef8d42d1f82e3e1be
Alvaro <alvaroflmiranda@gmail.com>
2024-01-11 18:14:35 -0300

    Update dropdown.js
    fixes menu calculation while infering direction

--- src/definitions/modules/dropdown.js
+++ src/definitions/modules/dropdown.js
@@ -3654,6 +3654,7 @@
                             ? callback
                             : function () {};
                         module.verbose('Doing menu show animation', $currentMenu);
+                        $currentMenu.removeClass(className.hidden);
                         module.set.direction($subMenu);
                         transition = settings.transition.showMethod || module.get.transition($subMenu);
                         if (module.is.selection()) {
