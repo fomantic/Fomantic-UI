13b76ee714bba8efdeef8a6e402cda6774abce40
Michael Voříšek <mvorisek@mvorisek.cz>
2024-01-29 19:02:28 +0100

    feat(lint): enforce NL before opening curly braces

--- src/definitions/modules/progress.js
+++ src/definitions/modules/progress.js
@@ -791,7 +791,9 @@
                             });
                         }
                         clearTimeout(module.performance.timer);
-                        module.performance.timer = setTimeout(function () { module.performance.display(); }, 500);
+                        module.performance.timer = setTimeout(function () {
+                            module.performance.display();
+                        }, 500);
                     },
                     display: function () {
                         var
