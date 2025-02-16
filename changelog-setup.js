411f1e2d6e0e079932126607e8684d65162a42ed
lubber-de <c64@lubber.de>
2025-02-16 10:10:39 +0100

    feat(chore): new changelog wip

--- changelog-setup.js
+++ changelog-setup.js
@@ -1,7 +1,9 @@
 const semver = require('semver'); // eslint-disable-line import/no-extraneous-dependencies
 
+let changelogDeps  = {};
+
 module.exports = function (Handlebars) {
-    Handlebars.registerHelper('commit-list-heading-only', (context, options) => {
+    Handlebars.registerHelper('commit-list-enhanced', (context, options) => {
         const {
             exclude,
             message,
@@ -13,7 +15,7 @@ module.exports = function (Handlebars) {
             return '';
         }
 
-        const list = context
+        let list = context
             .filter((item) => {
                 const commit = item.commit || item;
                 if (exclude) {
@@ -35,30 +37,77 @@ module.exports = function (Handlebars) {
 
                 return true;
             })
-            .map((item) => options.fn(item))
-            .join('');
+            .map((item) => options.fn(item));
 
-        if (!list) {
+        if (list.length === 0) {
             return '';
         }
 
-        return `${heading}\n\n`;
+        list = list.filter((item) => item.trim() !== '').join('');
+
+        let returnString = '';
+        if (heading) {
+            returnString = `${heading}\n\n`;
+        }
+        if (list) {
+            returnString += `${list}\n`;
+        }
+
+        return returnString;
     });
 
-    Handlebars.registerHelper('noprefix', function (text) {
-        var result = text.replace(/^(fix|feat|build)\(.*\): */, '');
+    Handlebars.registerHelper('noprefix', (text) => {
+        if (!(typeof text === 'string')) {
+            return '';
+        }
+        let result = text.replace(/^(fix|feat|build)\(.*\): */, '');
+
         return new Handlebars.SafeString(result);
     });
 
-    Handlebars.registerHelper('commit-list-heading-only', (context, options) => {
-        const { exclude, message, subject, heading } = options.hash;
-        let changelogDeps  = {};
+    Handlebars.registerHelper('commit-list-dependencies', (context, options) => {
+        const {
+            exclude,
+            message,
+            subject,
+            heading
+        } = options.hash;
 
         if (!context || context.length === 0 || !heading) {
             return '';
         }
 
-        const list = context
+        changelogDeps = {};
+        const
+            depsRegex = /build\(deps.*\): bump (.*) from (\d+\.\d.+\.\d+) to (\d+\.\d.+\.\d+)/,
+            detectVersionRange = function (item) {
+                let subjectDetails = item.subject.match(depsRegex);
+                if (!subjectDetails) {
+                    return false;
+                }
+
+                let depPackage = subjectDetails[1],
+                    depVersionFrom = subjectDetails[2],
+                    depVersionTo = subjectDetails[3]
+                ;
+                if (!changelogDeps[depPackage]) {
+                    changelogDeps[depPackage] = {
+                        from: '999.999.999',
+                        to: '0.0.0'
+                    };
+                }
+                if (semver.lt(depVersionFrom, changelogDeps[depPackage].from)) {
+                    changelogDeps[depPackage].from = depVersionFrom;
+                }
+                if (semver.gte(depVersionTo, changelogDeps[depPackage].to)) {
+                    changelogDeps[depPackage].to = depVersionTo;
+
+                    return true;
+                }
+
+                return false;
+            },
+            list = context
             .filter(item => {
                 const commit = item.commit || item;
                 if (exclude) {
@@ -79,49 +128,31 @@ module.exports = function (Handlebars) {
                 }
 
                 return true;
-            }) /*
+            })
+            .filter(detectVersionRange)
+            // second round as the previous list came unordered from git
+            .filter(detectVersionRange)
+            // adjust from version to create the whole range in one line (linked to the latest commit)
             .map(item => {
-                let subjectDetails = item.subject.match(/build\(deps.*\): bump (.*) from.*to (.*)/),
+                let subjectDetails = item.subject.match(depsRegex),
                     depPackage = subjectDetails[1],
-                    depVersion = subjectDetails[2],
-                    existingVersion = changelogDeps[depPackage] || '0.0.0',
-                    returnValue = 'abc '
+                    depVersionFrom = subjectDetails[2]
                 ;
-                if (semver.gt(depVersion, existingVersion)) {
-                    changelogDeps[depPackage] = existingVersion;
-                    returnValue = depPackage + ' to ' + depPackage;
-                }
-            }) */
+                item.subject = item.subject.replace(depVersionFrom, changelogDeps[depPackage].from);
+
+                return item;
+            })
             .map((item) => options.fn(item))
-            .join('')
-        ;
+            .join('');
 
         if (!list) {
             return '';
         }
 
         if (!heading) {
-            return list;
+            return `${list}\n`;
         }
 
-        return `${heading}\n\n${list}`;
-
-        // build\(deps.*\): bump (.*) from.*to (.*)
-
-        if (!context || !context.subject) {
-            return '';
-        }
-        let subjectDetails = context.subject.match(/build\(deps.*\): bump (.*) from.*to (.*)/),
-            depPackage = subjectDetails[1],
-            depVersion = subjectDetails[2],
-            existingVersion = changelogDeps[depPackage] || '0.0.0',
-            returnValue = 'abc '
-        ;
-        if (semver.gt(depVersion, existingVersion)) {
-            changelogDeps[depPackage] = existingVersion;
-            returnValue = depPackage + ' to ' + depPackage;
-        }
-
-        return returnValue;
+        return `${heading}\n\n${list}\n`;
     })
 };
