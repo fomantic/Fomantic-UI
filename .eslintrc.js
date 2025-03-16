a0854fd651a0ddae697ca1850aff0c70f86c9d75
Michael Voříšek <mvorisek@mvorisek.cz>
2025-03-16 10:31:56 +0100

    "no-var" is then fixed

--- .eslintrc.js
+++ .eslintrc.js
@@ -86,7 +86,6 @@ module.exports = {
         'guard-for-in': 'off', // refactor to "for of"
         'no-restricted-globals': 'off',
         'no-restricted-properties': 'off',
-        'no-var': 'off', // fixed in https://github.com/fomantic/Fomantic-UI/pull/2584 expect shadowed variables
         'prefer-const': 'off',
         'prefer-exponentiation-operator': 'off',
         'prefer-rest-params': 'off',
