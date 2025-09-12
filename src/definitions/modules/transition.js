fomantic/Fomantic-UI

commit f43094d1fb08366560d4e5d5ec06134be1612ba4

feat(lint): prefer-rest-params, prefer-spread, prefer-prototype-methods
switch to rest params, spread syntax as well as prototype usage which in turn enables the default eslint unicorn rules

src/definitions/modules/transition.js
@@ -19,16 +19,15 @@
         ? window
         : globalThis;
 
-    $.fn.transition = function () {
+    $.fn.transition = function (...args) {
         let $allModules = $(this);
 
         let time = Date.now();
         let performance = [];
 
-        let moduleArguments = arguments;
-        let query = moduleArguments[0];
-        let queryArguments = [].slice.call(arguments, 1);
-        let methodInvoked = typeof query === 'string';
+        let parameters = args[0];
+        let queryArguments = args.slice(1);
+        let methodInvoked = typeof parameters === 'string';
 
         let returnedValue;
         $allModules.each(function (index) {
@@ -51,7 +50,7 @@
 
                 initialize: function () {
                     // get full settings
-                    settings = module.get.settings.apply(element, moduleArguments);
+                    settings = module.get.settings.apply(element, parameters);
 
                     // shorthand
                     className = settings.className;
@@ -64,7 +63,7 @@
                     instance = $module.data(moduleNamespace) || module;
 
                     if (methodInvoked) {
-                        methodInvoked = module.invoke(query);
+                        methodInvoked = module.invoke(parameters);
                     }
 
                     // method not invoked, lets run an animation
@@ -756,30 +755,30 @@
                         return module[name];
                     }
                 },
-                debug: function () {
+                debug: function (...args) {
                     if (!settings.silent && settings.debug) {
                         if (settings.performance) {
-                            module.performance.log(arguments);
+                            module.performance.log(args);
                         } else {
                             module.debug = Function.prototype.bind.call(console.info, console, settings.name + ':');
-                            module.debug.apply(console, arguments);
+                            module.debug.apply(console, args);
                         }
                     }
                 },
-                verbose: function () {
+                verbose: function (...args) {
                     if (!settings.silent && settings.verbose && settings.debug) {
                         if (settings.performance) {
-                            module.performance.log(arguments);
+                            module.performance.log(args);
                         } else {
                             module.verbose = Function.prototype.bind.call(console.info, console, settings.name + ':');
-                            module.verbose.apply(console, arguments);
+                            module.verbose.apply(console, args);
                         }
                     }
                 },
-                error: function () {
+                error: function (...args) {
                     if (!settings.silent) {
                         module.error = Function.prototype.bind.call(console.error, console, settings.name + ':');
-                        module.error.apply(console, arguments);
+                        module.error.apply(console, args);
                     }
                 },
                 performance: {
@@ -794,7 +793,7 @@
                             time = currentTime;
                             performance.push({
                                 Name: message[0],
-                                Arguments: [].slice.call(message, 1) || '',
+                                Arguments: message.slice(1),
                                 Element: element,
                                 'Execution Time': executionTime,
                             });

.eslintrc.js
@@ -88,16 +88,13 @@ module.exports = {
         'no-restricted-properties': 'off',
         'prefer-const': 'off',
         'prefer-exponentiation-operator': 'off',
-        'prefer-rest-params': 'off',
         'unicorn/no-array-for-each': 'off',
         'unicorn/no-for-loop': 'off', // autofixes to "for of"
         'unicorn/prefer-code-point': 'off',
         'unicorn/prefer-includes': 'off',
         'unicorn/prefer-number-properties': 'off',
         'unicorn/prefer-optional-catch-binding': 'off',
-        'unicorn/prefer-prototype-methods': 'off',
         'unicorn/prefer-reflect-apply': 'off',
-        'unicorn/prefer-spread': 'off',
         'unicorn/prefer-top-level-await': 'off', // needs Node 14+
         'vars-on-top': 'off',
 

changelog-setup.js
@@ -18,7 +18,7 @@ const issueLinks = function (item) {
 module.exports = function (Handlebars) {
     Handlebars.registerHelper('commit-collector', (merges, commits, major, minor, options) => {
         const commitsFromMerges = merges.map((merge) => merge.commit);
-        const result = commits.concat(commitsFromMerges);
+        const result = [...commits, ...commitsFromMerges];
         isMajor = major;
         isMinor = minor;
 

src/definitions/behaviors/api.js
@@ -23,7 +23,7 @@
         ? window
         : globalThis;
 
-    $.fn.api = function (parameters) {
+    $.fn.api = function (...args) {
         // use window context if none specified
         let $allModules = isFunction(this)
             ? $(window)
@@ -31,9 +31,9 @@
         let time = Date.now();
         let performance = [];
 
-        let query = arguments[0];
-        let methodInvoked = typeof query === 'string';
-        let queryArguments = [].slice.call(arguments, 1);
+        let parameters = args[0];
+        let methodInvoked = typeof parameters === 'string';
+        let queryArguments = args.slice(1);
         let contextCheck = function (context, win) {
             let $context;
             if ([window, document].indexOf(context) >= 0) {
@@ -870,30 +870,30 @@
                         return module[name];
                     }
                 },
-                debug: function () {
+                debug: function (...args) {
                     if (!settings.silent && settings.debug) {
                         if (settings.performance) {
-                            module.performance.log(arguments);
+                            module.performance.log(args);
                         } else {
                             module.debug = Function.prototype.bind.call(console.info, console, settings.name + ':');
-                            module.debug.apply(console, arguments);
+                            module.debug.apply(console, args);
                         }
                     }
                 },
-                verbose: function () {
+                verbose: function (...args) {
                     if (!settings.silent && settings.verbose && settings.debug) {
                         if (settings.performance) {
-                            module.performance.log(arguments);
+                            module.performance.log(args);
                         } else {
                             module.verbose = Function.prototype.bind.call(console.info, console, settings.name + ':');
-                            module.verbose.apply(console, arguments);
+                            module.verbose.apply(console, args);
                         }
                     }
                 },
-                error: function () {
+                error: function (...args) {
                     if (!settings.silent) {
                         module.error = Function.prototype.bind.call(console.error, console, settings.name + ':');
-                        module.error.apply(console, arguments);
+                        module.error.apply(console, args);
                     }
                 },
                 performance: {
@@ -908,7 +908,7 @@
                             time = currentTime;
                             performance.push({
                                 Name: message[0],
-                                Arguments: [].slice.call(message, 1) || '',
+                                Arguments: message.slice(1),
                                 // 'Element'        : element,
                                 'Execution Time': executionTime,
                             });
@@ -989,7 +989,7 @@
                 if (instance === undefined) {
                     module.initialize();
                 }
-                module.invoke(query);
+                module.invoke(parameters);
             } else {
                 if (instance !== undefined) {
                     instance.invoke('destroy');

src/definitions/behaviors/form.js
@@ -19,16 +19,16 @@
         ? window
         : globalThis;
 
-    $.fn.form = function (parameters) {
+    $.fn.form = function (...args) {
         let $allModules = $(this);
         let $window = $(window);
 
         let time = Date.now();
         let performance = [];
 
-        let query = arguments[0];
-        let methodInvoked = typeof query === 'string';
-        let queryArguments = [].slice.call(arguments, 1);
+        let parameters = args[0];
+        let methodInvoked = typeof parameters === 'string';
+        let queryArguments = args.slice(1);
         let returnedValue;
         $allModules.each(function () {
             let $module = $(this);
@@ -79,7 +79,7 @@
                         if (instance === undefined) {
                             module.instantiate();
                         }
-                        module.invoke(query);
+                        module.invoke(parameters);
                     } else {
                         if (instance !== undefined) {
                             instance.invoke('destroy');
@@ -1337,7 +1337,7 @@
                             }
                         } else {
                             if (showErrors && fieldErrors.length > 0) {
-                                formErrors = formErrors.concat(fieldErrors);
+                                formErrors = [...formErrors, ...fieldErrors];
                                 module.add.prompt(identifier, fieldErrors, true);
                                 settings.onInvalid.call($field, fieldErrors);
                             }
@@ -1406,36 +1406,36 @@
                         return module[name];
                     }
                 },
-                debug: function () {
+                debug: function (...args) {
                     if (!settings.silent && settings.debug) {
                         if (settings.performance) {
-                            module.performance.log(arguments);
+                            module.performance.log(args);
                         } else {
                             module.debug = Function.prototype.bind.call(console.info, console, settings.name + ':');
-                            module.debug.apply(console, arguments);
+                            module.debug.apply(console, args);
                         }
                     }
                 },
-                verbose: function () {
+                verbose: function (...args) {
                     if (!settings.silent && settings.verbose && settings.debug) {
                         if (settings.performance) {
-                            module.performance.log(arguments);
+                            module.performance.log(args);
                         } else {
                             module.verbose = Function.prototype.bind.call(console.info, console, settings.name + ':');
-                            module.verbose.apply(console, arguments);
+                            module.verbose.apply(console, args);
                         }
                     }
                 },
-                error: function () {
+                error: function (...args) {
                     if (!settings.silent) {
                         module.error = Function.prototype.bind.call(console.error, console, settings.name + ':');
-                        module.error.apply(console, arguments);
+                        module.error.apply(console, args);
                     }
                 },
-                warn: function () {
+                warn: function (...args) {
                     if (!settings.silent) {
                         module.warn = Function.prototype.bind.call(console.warn, console, settings.name + ':');
-                        module.warn.apply(console, arguments);
+                        module.warn.apply(console, args);
                     }
                 },
                 performance: {
@@ -1450,7 +1450,7 @@
                             time = currentTime;
                             performance.push({
                                 Name: message[0],
-                                Arguments: [].slice.call(message, 1) || '',
+                                Arguments: message.slice(1),
                                 Element: element,
                                 'Execution Time': executionTime,
                             });

src/definitions/behaviors/state.js
@@ -19,15 +19,15 @@
         ? window
         : globalThis;
 
-    $.fn.state = function (parameters) {
+    $.fn.state = function (...args) {
         let $allModules = $(this);
 
         let time = Date.now();
         let performance = [];
 
-        let query = arguments[0];
-        let methodInvoked = typeof query === 'string';
-        let queryArguments = [].slice.call(arguments, 1);
+        let parameters = args[0];
+        let methodInvoked = typeof parameters === 'string';
+        let queryArguments = args.slice(1);
         let contextCheck = function (context, win) {
             let $context;
             if ([window, document].indexOf(context) >= 0) {
@@ -413,30 +413,30 @@
                         return module[name];
                     }
                 },
-                debug: function () {
+                debug: function (...args) {
                     if (!settings.silent && settings.debug) {
                         if (settings.performance) {
-                            module.performance.log(arguments);
+                            module.performance.log(args);
                         } else {
                             module.debug = Function.prototype.bind.call(console.info, console, settings.name + ':');
-                            module.debug.apply(console, arguments);
+                            module.debug.apply(console, args);
                         }
                     }
                 },
-                verbose: function () {
+                verbose: function (...args) {
                     if (!settings.silent && settings.verbose && settings.debug) {
                         if (settings.performance) {
-                            module.performance.log(arguments);
+                            module.performance.log(args);
                         } else {
                             module.verbose = Function.prototype.bind.call(console.info, console, settings.name + ':');
-                            module.verbose.apply(console, arguments);
+                            module.verbose.apply(console, args);
                         }
                     }
                 },
-                error: function () {
+                error: function (...args) {
                     if (!settings.silent) {
                         module.error = Function.prototype.bind.call(console.error, console, settings.name + ':');
-                        module.error.apply(console, arguments);
+                        module.error.apply(console, args);
                     }
                 },
                 performance: {
@@ -451,7 +451,7 @@
                             time = currentTime;
                             performance.push({
                                 Name: message[0],
-                                Arguments: [].slice.call(message, 1) || '',
+                                Arguments: message.slice(1),
                                 Element: element,
                                 'Execution Time': executionTime,
                             });
@@ -532,7 +532,7 @@
                 if (instance === undefined) {
                     module.initialize();
                 }
-                module.invoke(query);
+                module.invoke(parameters);
             } else {
                 if (instance !== undefined) {
                     instance.invoke('destroy');

src/definitions/behaviors/visibility.js
@@ -19,15 +19,15 @@
         ? window
         : globalThis;
 
-    $.fn.visibility = function (parameters) {
+    $.fn.visibility = function (...args) {
         let $allModules = $(this);
 
         let time = Date.now();
         let performance = [];
 
-        let query = arguments[0];
-        let methodInvoked = typeof query === 'string';
-        let queryArguments = [].slice.call(arguments, 1);
+        let parameters = args[0];
+        let methodInvoked = typeof parameters === 'string';
+        let queryArguments = args.slice(1);
         let contextCheck = function (context, win) {
             let $context;
             if ([window, document].indexOf(context) >= 0) {
@@ -177,9 +177,9 @@
                         }, 100);
                     },
                     contextChanged: function (mutations) {
-                        [].forEach.call(mutations, function (mutation) {
+                        Array.prototype.forEach.call(mutations, function (mutation) {
                             if (mutation.removedNodes) {
-                                [].forEach.call(mutation.removedNodes, function (node) {
+                                Array.prototype.forEach.call(mutation.removedNodes, function (node) {
                                     if (node === element || $(node).find(element).length > 0) {
                                         module.debug('Element removed from DOM, tearing down events');
                                         module.destroy();
@@ -971,30 +971,30 @@
                         return module[name];
                     }
                 },
-                debug: function () {
+                debug: function (...args) {
                     if (!settings.silent && settings.debug) {
                         if (settings.performance) {
-                            module.performance.log(arguments);
+                            module.performance.log(args);
                         } else {
                             module.debug = Function.prototype.bind.call(console.info, console, settings.name + ':');
-                            module.debug.apply(console, arguments);
+                            module.debug.apply(console, args);
                         }
                     }
                 },
-                verbose: function () {
+                verbose: function (...args) {
                     if (!settings.silent && settings.verbose && settings.debug) {
                         if (settings.performance) {
-                            module.performance.log(arguments);
+                            module.performance.log(args);
                         } else {
                             module.verbose = Function.prototype.bind.call(console.info, console, settings.name + ':');
-                            module.verbose.apply(console, arguments);
+                            module.verbose.apply(console, args);
                         }
                     }
                 },
-                error: function () {
+                error: function (...args) {
                     if (!settings.silent) {
                         module.error = Function.prototype.bind.call(console.error, console, settings.name + ':');
-                        module.error.apply(console, arguments);
+                        module.error.apply(console, args);
                     }
                 },
                 performance: {
@@ -1009,7 +1009,7 @@
                             time = currentTime;
                             performance.push({
                                 Name: message[0],
-                                Arguments: [].slice.call(message, 1) || '',
+                                Arguments: message.slice(1),
                                 Element: element,
                                 'Execution Time': executionTime,
                             });
@@ -1092,7 +1092,7 @@
                 }
                 instance.save.scroll();
                 instance.save.calculations();
-                module.invoke(query);
+                module.invoke(parameters);
             } else {
                 if (instance !== undefined) {
                     instance.invoke('destroy');

src/definitions/globals/site.js
@@ -19,13 +19,13 @@
         ? window
         : globalThis;
 
-    $.fn.site = function (parameters) {
+    $.fn.site = function (...args) {
         let time = Date.now();
         let performance = [];
 
-        let query = arguments[0];
-        let methodInvoked = typeof query === 'string';
-        let queryArguments = [].slice.call(arguments, 1);
+        let parameters = args[0];
+        let methodInvoked = typeof parameters === 'string';
+        let queryArguments = args.slice(1);
 
         let settings = $.isPlainObject(parameters)
             ? $.extend(true, {}, $.site.settings, parameters)
@@ -236,29 +236,29 @@
                     return module[name];
                 }
             },
-            debug: function () {
+            debug: function (...args) {
                 if (settings.debug) {
                     if (settings.performance) {
-                        module.performance.log(arguments);
+                        module.performance.log(args);
                     } else {
                         module.debug = Function.prototype.bind.call(console.info, console, settings.name + ':');
-                        module.debug.apply(console, arguments);
+                        module.debug.apply(console, args);
                     }
                 }
             },
-            verbose: function () {
+            verbose: function (...args) {
                 if (settings.verbose && settings.debug) {
                     if (settings.performance) {
-                        module.performance.log(arguments);
+                        module.performance.log(args);
                     } else {
                         module.verbose = Function.prototype.bind.call(console.info, console, settings.name + ':');
-                        module.verbose.apply(console, arguments);
+                        module.verbose.apply(console, args);
                     }
                 }
             },
-            error: function () {
+            error: function (...args) {
                 module.error = Function.prototype.bind.call(console.error, console, settings.name + ':');
-                module.error.apply(console, arguments);
+                module.error.apply(console, args);
             },
             performance: {
                 log: function (message) {
@@ -273,7 +273,7 @@
                         performance.push({
                             Element: element,
                             Name: message[0],
-                            Arguments: [].slice.call(message, 1) || '',
+                            Arguments: message.slice(1),
                             'Execution Time': executionTime,
                         });
                     }
@@ -353,7 +353,7 @@
             if (instance === undefined) {
                 module.initialize();
             }
-            module.invoke(query);
+            module.invoke(parameters);
         } else {
             if (instance !== undefined) {
                 module.destroy();

commit 63d966fc7b647841d667d2be1345489d2c16f17c

fix(transition): args usage was broken inside settings generator
transition was broken since #3341 which this PR fixes

src/definitions/modules/transition.js
@@ -25,6 +25,7 @@
         let time = Date.now();
         let performance = [];
 
+        let moduleArguments = args;
         let parameters = args[0];
         let queryArguments = args.slice(1);
         let methodInvoked = typeof parameters === 'string';
@@ -50,7 +51,7 @@
 
                 initialize: function () {
                     // get full settings
-                    settings = module.get.settings.apply(element, parameters);
+                    settings = module.get.settings(...moduleArguments);
 
                     // shorthand
                     className = settings.className;